71b90bd2b0509dbf90ab3e3c75df3e85a8b5c67c
zhaolong <929034478@qq.com>
2015-08-14 14:03:39 +0800

    template for addMoview

--- movie/app.js
+++ movie/app.js
@@ -1,22 +1,29 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./routes');
 var users = require('./routes/users');
-
+var movie = require('./routes/movie');
+var http = require('http');
+var ejs = require('ejs');
 var app = express();
+// var SessionStore = require("session-mongoose")(express);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
+//使用html模版引擎
+app.engine('.html',ejs.__express);
+app.set('view engine', 'html');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger('dev')); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -24,6 +31,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.get('/movie/add', movie.movieAdd);
+app.post('/movie/add', movie.doMovieAdd);
+app.get('/movie/:name', movie.movieAdd);
+// app.get('/movie/json/:name', movie.movieJSON);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +67,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.listen(3000);
 
 module.exports = app;
